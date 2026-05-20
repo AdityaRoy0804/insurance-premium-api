201a57e5e28d3ec4ccaf9c425a70bf2908be7c14
AdityaRoy0804 <roy97278@gmail.com>
2026-05-21 01:36:36 +0530

    added CORSMiddleware for resource sharing

--- api.py
+++ api.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from model.predict import predict_insurance_premium, load_model_check
 from schema.input_validation import user_input
 
 app = FastAPI()
+
+# Allow CORS for respective origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5501", "http://localhost:5501"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# API Endpoints and Routes
 
 @app.get("/")
 def home():
