3e814af945afa35ebff088a926c48528ad9137e2
AdityaRoy0804 <roy97278@gmail.com>
2026-05-20 17:55:01 +0530

    updated the pydantic model

--- schema/input_validation.py
+++ schema/input_validation.py
@@ -41,10 +41,16 @@ class user_input(BaseModel):
     @computed_field
     @property
     def lifestyle_risk(self) -> str:
-        if self.smoker and self.bmi > 30:
+
+        if self.smoker and self.bmi >= 30:
             return "high"
-        elif self.smoker or self.bmi > 27:
+
+        elif (
+            (self.smoker and self.bmi >= 26)
+            or (not self.smoker and self.bmi >= 30)
+        ):
             return "medium"
+
         else:
             return "low"
         
