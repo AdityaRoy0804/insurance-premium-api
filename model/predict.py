77ff23c27d09145ef51531392a622490c1376cca
AdityaRoy0804 <roy97278@gmail.com>
2026-05-20 16:59:28 +0530

    created function to calculate premium and confidence score

---
+++ model/predict.py
@@ -0,0 +1,29 @@
+import joblib
+import pandas as pd
+
+# load the trained model
+model_path = "../model/insurance_premium_model.pkl"
+with open(model_path, "rb") as f:
+    model = joblib.load(f)
+
+# get classes from the model(for confidence score)
+class_labels = model.classes_.tolist()
+
+# function to make predictions
+def predict_insurance_premium(input_data: dict):
+    # Convert input data to DataFrame
+    input_df = pd.DataFrame([input_data])
+    
+    # Make prediction
+    predicted_premium = model.predict(input_df)[0]
+    
+    # Get confidence scores for each class
+    confidence_scores = model.predict_proba(input_df)[0]
+    
+    # Create a dictionary of class labels and their corresponding confidence scores
+    confidence_dict = {class_labels[i]: confidence_scores[i] for i in range(len(class_labels))}
+    
+    return {
+        "predicted_premium": predicted_premium,
+        "confidence_scores": confidence_dict
+    } 
