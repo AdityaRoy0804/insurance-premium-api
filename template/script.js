47de62904a307f191db83d36e2f43392bcf915ad
AdityaRoy0804 <roy97278@gmail.com>
2026-05-22 00:14:43 +0530

    api url changed so it routes through nginx

--- template/script.js
+++ template/script.js
@@ -35,7 +35,7 @@ form.addEventListener("submit", async (e) => {
 
     try {
 
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("/api/predict", {
 
             method: "POST",
 
